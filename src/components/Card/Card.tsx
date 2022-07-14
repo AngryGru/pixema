810ec9f695029de443ae0cbc9b9daee8c59f8711
Anna Gruzd <ann.gruzd97.ag@gmail.com>
2022-07-14 21:09:41 +0300

    feat: add Card and CardList component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import classNames from "classnames";
+import "./Card.scss";
+import { MovieCardType } from "../../common/types";
+import FireIcon from "../../assets/icons/FireIcon";
+
+const Card: FC<MovieCardType> = ({
+  poster,
+  name,
+  tagline,
+  rating,
+  isTrends,
+}) => {
+  return (
+    <div className="cardContainer">
+      <div className="cardContent">
+        <div className="cardPoster">
+          {rating && (
+            <div
+              className={classNames("raiting", {
+                ["highRaiting"]: +rating >= 7,
+                ["midRaiting"]: +rating >= 4 && +rating < 7,
+                ["lowRaiting"]: +rating < 4,
+              })}
+            >
+              {isTrends && <FireIcon />}
+              {rating}
+            </div>
+          )}
+
+          <img src={poster} alt="movie poster" />
+        </div>
+        <div className="cardText">
+          <p className="cardTitle">{name}</p>
+          <p className="cardDescription">{tagline}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import "./CardList.scss";
+import { MovieCardType } from "../../common/types";
+import Card from "../Card";
+
+type CardListProps = {
+  data: MovieCardType[];
+};
+
+const CardList: FC<CardListProps> = ({ data }: any) => {
+  const navigate = useNavigate();
+
+  const onCardClick = (id: string) => {
+    navigate(`/cards-list/${id}`);
+  };
+
+  const listCards = data.map((item: MovieCardType) => {
+    return (
+      <div key={item.id} onClick={() => onCardClick(item.id.toString())}>
+        <Card
+          id={item.id}
+          key={item.id}
+          poster={item.poster}
+          name={item.name}
+          tagline={item.tagline}
+          rating={item.rating}
+        />
+      </div>
+    );
+  });
+  return <div className="cardListContainer">{listCards}</div>;
+};
+
+export default CardList;
